5b8a9c195886967e525514ea746dae0ddc4ed192
Hans Adema <git@xvh.nl>
2017-05-21 17:00:19 +0200

    Reduce the number of available strategies

--- meet_elasticsearch/query.py
+++ meet_elasticsearch/query.py
@@ -1,4 +1,7 @@
 def base(term):
+    """
+    Completely standard Elasticsearch behaviour without any customization.
+    """
     return {
         "query": {
             "multi_match": {
@@ -10,6 +13,9 @@ def base(term):
 
 
 def fuzzy(term):
+    """
+    Search the regular name field with Fuzzy matching
+    """
     return {
         "query": {
             "multi_match": {
@@ -22,6 +28,9 @@ def fuzzy(term):
 
 
 def trigram_single_match(term):
+    """
+    Search only the Trigram field (n-gram with length 3)
+    """
     return {
         "query": {
             "multi_match": {
@@ -33,6 +42,9 @@ def trigram_single_match(term):
 
 
 def trigram_multi_match(term):
+    """
+    Search the Trigram field and regular name field and combine them with the default "best fields" method.
+    """
     return {
         "query": {
             "multi_match": {
@@ -43,157 +55,75 @@ def trigram_multi_match(term):
     }
 
 
-def bigram_single_match(term):
+def trigram_most_fields(term):
+    """
+    Search the trigram field and regular name field and combine them with the "most fields" method.
+    """
     return {
         "query": {
             "multi_match": {
                 'query': term,
-                'fields': ['name.bigram']
-            }
-        }
-    }
-
-
-def bigram_multi_match(term):
-    return {
-        "query": {
-            "multi_match": {
-                'query': term,
-                'fields': ['name', 'name.bigram']
-            }
-        }
-    }
-
-
-def bigram_most_fields(term):
-    return {
-        "query": {
-            "multi_match": {
-                'query': term,
-                'fields': ['name', 'name.bigram'],
+                'fields': ['name', 'name.trigram'],
                 'type': 'most_fields'
             }
         }
     }
 
 
-def bigram_cross_fields(term):
+def trigram_cross_fields(term):
+    """
+    Search the trigram field and regular name field and combine them with the "cross fields" method.
+    """
     return {
         "query": {
             "multi_match": {
                 'query': term,
-                'fields': ['name', 'name.bigram'],
+                'fields': ['name', 'name.trigram'],
                 'type': 'cross_fields'
             }
         }
     }
 
 
-def bigram_and(term):
+def trigram_phrase(term):
+    """
+    Search the trigram field and regular name field and combine then with the "phrase" method
+    """
     return {
         "query": {
             "multi_match": {
                 'query': term,
-                'fields': ['name', 'name.bigram'],
-                'operator': 'and'
-            }
-        }
-    }
-
-
-def bigram_most_fields_and(term):
-    return {
-        "query": {
-            "multi_match": {
-                'query': term,
-                'fields': ['name', 'name.bigram'],
-                'type': 'most_fields',
-                'operator': 'and'
-            }
-        }
-    }
-
-
-def bigram_most_fields_and_fuzzy(term):
-    return {
-        "query": {
-            "multi_match": {
-                'query': term,
-                'fields': ['name', 'name.bigram'],
-                'type': 'most_fields',
-                'operator': 'and',
-                'fuzziness': 'AUTO'
-            }
-        }
-    }
-
-
-def bigram_cross_fields_and(term):
-    return {
-        "query": {
-            "multi_match": {
-                'query': term,
-                'fields': ['name', 'name.bigram'],
-                'type': 'cross_fields',
-                'operator': 'and'
-            }
-        }
-    }
-
-
-def bigram_phrase(term):
-    return {
-        "query": {
-            "multi_match": {
-                'query': term,
-                'fields': ['name', 'name.bigram'],
+                'fields': ['name', 'name.trigram'],
                 'type': 'phrase'
             }
         }
     }
 
 
-def bigram_phrase_prefix(term):
+def trigram_boosted_name(term):
+    """
+    Search the trigram field and regular name field and boost the name field.
+    """
     return {
         "query": {
             "multi_match": {
                 'query': term,
-                'fields': ['name', 'name.bigram'],
-                'type': 'phrase_prefix'
-            }
-        }
-    }
-
-
-def bigram_trigram(term):
-    return {
-        "query": {
-            "multi_match": {
-                'query': term,
-                'fields': ['name', 'name.bigram', 'name.trigram']
-            }
-        }
-    }
-
-
-def bigram_boosted_name(term):
-    return {
-        "query": {
-            "multi_match": {
-                'query': term,
-                'fields': ['name^5', 'name.bigram'],
+                'fields': ['name^5', 'name.trigram'],
                 'type': 'most_fields'
             }
         }
     }
 
 
-def bigram_boosted_bigram(term):
+def trigram_boosted_trigram(term):
+    """
+    Search the trigram field and regular name field and boost the trigram field.
+    """
     return {
         "query": {
             "multi_match": {
                 'query': term,
-                'fields': ['name', 'name.bigram^5'],
+                'fields': ['name', 'name.trigram^5'],
                 'type': 'most_fields'
             }
         }
